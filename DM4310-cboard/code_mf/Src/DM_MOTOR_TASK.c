cf1db8d80f03c89be3e0268d9f4a074e5883f710
GruBaXu <108203631+LTGBLSN@users.noreply.github.com>
2026-01-12 19:36:03 +0800

    update

---
+++ DM4310-cboard/code_mf/Src/DM_MOTOR_TASK.c
@@ -0,0 +1,24 @@
+//
+// Created by 21481 on 2025/12/15.
+//
+
+#include "cmsis_os.h"
+#include "dm_motor.h"
+#include "main.h"
+
+void DM_MOTOR_TASK()
+{
+    osDelay(2000);
+    dm_motor_mode_set(CMD_MOTOR_MODE, DM4310_01);
+
+    while (1)
+    {
+
+        DM4310_01.give_tor = (float )rc_ch1*0.002f ;
+//        DM4310_01.give_tor = 0.5f ;
+
+
+
+        osDelay(1);
+    }
+}
